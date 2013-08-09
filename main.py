a176423f3129f2da64f2de9eb54b98d0395d82b1
zm <zmwlxez@gmail.com>
2013-08-09 19:53:39 +0800

    .

--- main.py
+++ main.py
@@ -42,11 +42,26 @@ class SubPost1(tornado.web.RequestHandler):
         print 'cookie: ', cookie
         print 's_cookie: ', s_cookie
 
+
+class FormHandler(tornado.web.RequestHandler):
+    def get(self):
+        print 'form get'
+        self.render('form.html')
+
+    def post(self):
+        t_1 = self.get_argument('t_1', None)
+        print 't_1: ', t_1
+        print 'form post'
+        self.redirect('/')
+
+
 settings = {'cookie_secret': '32143232'}
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r'/1', SubHandler),
-    (r'/s/1', SubPost1)],
+    (r'/s/1', SubPost1),
+    (r'/form', FormHandler),
+],
     **settings)
 
 if __name__ == "__main__":
